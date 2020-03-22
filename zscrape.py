6646428ca434df3142bf27614ff482f03cd6dfe0
Zack Adlington <zadlington@gmail.com>
2020-03-22 09:22:37 +0000

    Re-designed to OOP principles; added try-except for geocoding API_KEY

--- zscrape.py
+++ zscrape.py
@@ -1,229 +1,213 @@
-# CLI app to scrape job details (what, where) from Indeed into a new table in db.
+#!/usr/bin/env python3
+"""
+ZScrape: scrape job listings from Indeed.com.
+"""
+
+__author__ = "Zack Adlington"
+__version__ = "0.1.0"
+__license__ = "MIT"
 
 from bs4 import BeautifulSoup
-from cs50 import SQL
-import requests
-import re
-import string
-import datetime
-import time
-import argparse
-import sys
+from tqdm import tqdm
+from retrying import retry
 from price_parser import Price
 from geopy.geocoders import GoogleV3
-import geopy.geocoders
+import requests
+import argparse
 import os
-import logging
-from tqdm import tqdm
+import sys
 
-KEY = os.getenv('API_KEY')
+URL = "http://www.indeed.co.uk"
 
-def main():
-    # Set logging preferences
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    logging.getLogger("geopy").setLevel(logging.WARNING)
+def main(args):
+    """ Main entry point of the app """
+    scrapeAllJobAds(args.what, args.where)
 
-    # Command line help
-    parser = argparse.ArgumentParser(description = 'A web scraper for Indeed.com')
-    parser.add_argument('--what', required = 'True', help = 'Job search team, e.g. "software developer"')
-    parser.add_argument('--where', required = 'True', help = 'Location, e.g. "Bristol".')
-    args = parser.parse_args()
+def scrapeAllJobAds(what, where):
+    '''Returns list of JobAd instances scraped from Indeed for given args'''
+    job_ads = []
+    first_results_page = ResultsPage(0, what, where)    
+    total_job_count = getTotalResultCount(first_results_page.DOM)
 
-    if not KEY:
-        print("Error: Environmental variable 'API_KEY' must be set to a Google Maps credential.")
-        return 1
-
-    db_path = "jobs.db"
-
-    # Execute
-    vacancies = scrape(args.what, args.where)
-    vacancies = geocode(vacancies, args.where)
-    table_name = store(vacancies, args.what, args.where, db_path)
-
-def scrape(what, where):
-    '''
-    Function to scrape listings from Indeed.  Returns list of job vacancy dictionaries.
-    '''
-    url = "https://www.indeed.co.uk"
-
-    # Get total number of vacancies
-    page = 0
-    query = "/jobs?q=" + what + "&l=" + where + "&start=" + str(page)
-    site = requests.get(url + query)
-    whole_page = BeautifulSoup(site.text, "html.parser")
-    total_vacancies = whole_page.find(id="searchCountPages")
-    total_vacancies = int(total_vacancies.string.split()[3].replace(',', ''))
-
-    print("Scraping ~" + str(total_vacancies) + " vacancies for '" + what + "' in '" + where + "'...")
-    vacancies = []
-
-    index = 0
-    with tqdm(total = total_vacancies) as progress_bar:
+    with tqdm(total = total_job_count) as progress_bar:
+        page_counter = 0
+        job_counter = 0
         while True:
-            # Set page to scrape from
-            query = "/jobs?q=" + what + "&l=" + where + "&start=" + str(page)
-            site = requests.get(url + query)
-            whole_page = BeautifulSoup(site.text, "html.parser")
+            current_results_page = ResultsPage(page_counter, what, where)
+            result_elements = current_results_page.results_elements
+            for element in result_elements:         
+                # Do scraping (in JobAd's constructor method)
+                job_ads[job_counter] = JobAd(element)
 
-            for listing in whole_page.find_all(name="div", attrs={"class":"jobsearch-SerpJobCard"}):
-                vacancies.append({})
+                # Do geocoding if required
+                if args.geocode is True: 
+                    job_ads[job_counter].geocodeLocation
 
-                # Scrape job title
-                for title in listing.find_all(attrs={"class":"title"}):
-                    vacancies[index].update({"title" : str(listing.a["title"])})
-
-                # Scrape salary
-                if listing.find_all(name="span", attrs={"class":"salaryText"}):
-                    for div in listing.find_all(name="span", attrs={"class":"salaryText"}):
-                        vacancies[index].update({ "salary_raw" : div.string.strip("\n")})
-                    if "year" in vacancies[index]["salary_raw"] and " - " in vacancies[index]["salary_raw"]:
-                        bracket = vacancies[index]["salary_raw"].split(" - ")
-                        vacancies[index].update({ "salary_min" : Price.fromstring(bracket[0]).amount_float, "salary_max" : Price.fromstring(bracket[1]).amount_float })
-                        vacancies[index].update({ "salary_mid" : vacancies[index]["salary_max"] - (vacancies[index]["salary_max"] - vacancies[index]["salary_min"])/2 })
-                    elif "year" in vacancies[index]["salary_raw"]:
-                        annual_salary = vacancies[index]["salary_raw"].rstrip(" a year")
-                        vacancies[index].update({ "salary_mid" : Price.fromstring(annual_salary).amount_float })
-                        vacancies[index].update({ "salary_min" : None, "salary_max" : None })
-                    else:
-                        vacancies[index].update({"salary_raw" : None, "salary_min" : None, "salary_max" : None, "salary_mid" : None })
-                else:
-                    vacancies[index].update({"salary_raw" : None, "salary_min" : None, "salary_max" : None, "salary_mid" : None })
-
-                # Scrape company
-                for div in listing.find_all(attrs={"class":"sjcl"}):
-                    if div.find_all(name="a"):
-                        for company in div.find_all(name="a"):
-                            if company.string:
-                                vacancies[index].update({"company" : str(company.string.strip("\n"))})
-                    else:
-                        for company in div.find_all(name="span", attrs={"class":"company"}):
-                            vacancies[index].update({"company" : str(company.string.strip("\n"))})
-
-                # Scrape location
-                for location in listing.find_all(attrs={"class":"location"}):
-                    vacancies[index].update({"location" : str(location.string) })
-
-                # Scrape URL and get description from the page at that URL
-                for link in listing.find_all(attrs={"class":"title"}):
-                    vacancies[index].update({"link" : str(url + str(link.a.get("href")))})
-                    details = requests.get(url + link.a.get("href")) # Optimise here as Indeed redirects from this URL, reduce number of requests
-                    description = BeautifulSoup(details.text, "html.parser")
-
-                    for blurb in description.find_all(attrs={"id":"jobDescriptionText"}):
-                        text = str(blurb.contents).replace('"','\\\"')
-                        text = text.replace("'", "\\\'")
-                        vacancies[index].update({"description" : text })
-
-                index += 1
-
+                job_counter += 1
                 progress_bar.update(1)
 
-            # Check if on last page
-            if whole_page.find_all(name="span", attrs={"class":"np"}, string=re.compile("Next")):
-                page += 10
-                time.sleep(0.5)
+def getTotalResultCount(results_page):
+        '''Return int of total number of results'''
+        total_result_count = results_page.find(id="searchCountPages")
+        total_result_count = int(total_result_count.string.split()[3].replace(',', ''))
+        return total_result_count
+
+class ResultsPage():
+    def __init__(self, page_number, what, where):
+        self.DOM = self.getDOM(page_number, what, where)
+        self.results_elements = self.getResultElements(self.DOM)
+
+    @retry(wait_random_min=2000, wait_random_max=3000)
+    def getDOM(self, page_number, what, where):
+        '''Return BeautifulSoup object containing single page of results'''
+        query = "/jobs?q=" + what + "&l=" + where + "&start=" + str(page_number)  
+        site = requests.get(url + query)
+        site.raise_for_status()
+
+        return BeautifulSoup(site.text, "html.parser")
+
+    def getResultElements(self, results_page):
+        '''Returns iterable of individual result elements from results page'''
+        return results_page.find_all(name="div", attrs={"class":"jobsearch-SerpJobCard"})
+
+class JobAd():
+    def __init__(self, element):
+        self.title = self.getTitle(element)
+        self.company = self.getCompany(element)
+        self.raw_location = self.getRawLocation(element)
+        self.raw_salary = self.getRawSalary(element)
+        self.min_salary = self.parseMinSalary(element)
+        self.max_salary = self.parseMaxSalary(element)
+        self.mid_salary = self.calcMidSalary(element)
+        self.detail_url = self.getDetailURL(element)
+        self.description = self.getDescription(self.detail_url)
+        
+    def getTitle(self, element):
+        '''Return job title'''
+        return str(element.a["title"])
+        
+    def getCompany(self, element):
+        '''Return company'''
+        for div in element.find_all(attrs={"class":"sjcl"}):
+            if div.find_all(name="a"):
+                for company in div.find_all(name="a"):
+                    if company.string:
+                        return str(company.string.strip("\n"))
             else:
-                break
+                for company in div.find_all(name="span", attrs={"class":"company"}):
+                    return str(company.string.strip("\n"))
+    
+    def getRawLocation(self, element):
+        '''Return location as a raw string literal'''
+        location = element.find_all(attrs={"class":"location"})
+        return str(location.string)
 
-    print ('Scraping successful!')
-    return(vacancies)
+    def getRawSalary(self, element):
+        if element.find_all(name="span", attrs={"class":"salaryText"}):
+            for div in element.find_all(name="span", attrs={"class":"salaryText"}):
+                return div.string.strip("\n")
+        else:
+            return None
+    
+    def parseMinSalary(self, raw_salary):
+        '''
+        Returns lowest value in salary range or salary value if not a range
+        Disregards non-annual wages, e.g. 'per day' or 'per week' rates for contractors
+        '''
+        if raw_salary == None:
+            return None
+        else:
+            if "year" in raw_salary and " - " in raw_salary:
+                salary_range = raw_salary.split(" - ")
+                # salary_range[0] contains number to  left of " - ", lower value in range
+                min_salary = Price.fromstring(salary_range[0]).amount_float
+                return min_salary
+            elif "year" in raw_salary:
+                annual_salary = raw_salary.rstrip(" a year")
+                return annual_salary
+    
+    def parseMaxSalary(self, raw_salary):
+        '''
+        Returns highest value in salary range or salary value if not a range
+        Disregards non-annual wages, e.g. 'per day' or 'per week' rates for contractors
+        '''
+        if raw_salary == None:
+            return None
+        else:
+            if "year" in raw_salary and " - " in raw_salary:
+                salary_range = raw_salary.split(" - ")
+                # salary_range[0] contains number to  left of " - ", lower value in range
+                max_salary = Price.fromstring(salary_range[1]).amount_float
+                return max_salary
+            elif "year" in raw_salary:
+                annual_salary = raw_salary.rstrip(" a year")
+                return annual_salary
+    
+    def calcMidSalary(self, min_salary, max_salary):
+        '''Return mid point between min_salary and max_salary'''
+        return min_salary + (max_salary - min_salary) / 2
+        
+    def getDetailURL(self, element):
+        '''Return URL for page containing job listing detail'''
+        for link in element.find_all(attrs={"class":"title"}):
+            return str(URL + str(link.a.get("href")))
 
-def geocode(vacancies, where):
-    '''
-    Function to add lat and long to vacancy data using GeoPy and MapBox.
-    '''
-    geolocator = GoogleV3(api_key=KEY, domain='maps.google.co.uk')
-    city = geolocator.geocode(where, exactly_one = True)
+    def getDescription(self, detail_url):
+        details = requests.get(detail_url)
+        description = BeautifulSoup(details.text, "html.parser")
 
-    print ('Geocoding...')
+        for blurb in description.find_all(attrs={"id":"jobDescriptionText"}):
+            job_description = str(blurb.contents).replace('"','\\\"')
+            job_description = job_description.replace("'", "\\\'")
+            return job_description
+    
+    def geocodeLocation(self, element):
+        def geocodeCompany(self, element):
+            pass
+        
+        def geocodeLocation(self, element):
+            pass
+    
+        def geocodeCity(self, element):
+            pass
 
-    for index, listing in enumerate(tqdm(vacancies)):
-        company_location = geolocator.geocode((vacancies[index]["company"] + " " + vacancies[index]["location"]), exactly_one = True)
-        if company_location is None:
-            company_location = geolocator.geocode(vacancies[index]["location"], exactly_one = True)
-            if company_location is None:
-                company_location = city
-
-        vacancies[index].update({ "latitude" : company_location.latitude, "longitude" : company_location.longitude })
-
-        time.sleep(0.2)
-
-    print("Geocoding successful!")
-    return(vacancies)
-
-def store(vacancies, what, where, db_path):
-    '''
-    Function to move list of job vacancies to SQLite database
-    '''
-
-    db = SQL("sqlite:///" + db_path)
-
-    # Make args database friendly so they can be used to create the table name
-    pattern = re.compile('[\W_]+')
-    what_db = what; where_db = where
-    pattern.sub('', what_db)
-    pattern.sub('', where_db)
-
-    # Create new table
-    print("Adding to database...")
-    table = what_db + "_" + where_db + "_"
-    counter = 0
-    while counter < 20:
-        try:
-            db.execute("CREATE TABLE :table ("
-                            "job_id INTEGER PRIMARY KEY, "
-                            "title TEXT, "
-                            "company TEXT, "
-                            "salary_raw TEXT, "
-                            "salary_min REAL, "
-                            "salary_max REAL, "
-                            "salary_mid REAL, "
-                            "location TEXT, "
-                            "latitude TEXT, "
-                            "longitude TEXT, "
-                            "url TEXT, "
-                            "description TEXT)",
-                            table = table + str(counter))
-            break
+        try: 
+            geocodeCompany(element)
         except:
-            counter += 1
-    else:
-        print("Error: too many tables for this search term.  Try dropping some tables.")
-        return 1
-
-    table_name = table + str(counter)
-
-    # Record metadata
-    db.execute("INSERT INTO metadata ("
-                "'q_what', 'q_where', 'table_name') "
-            "VALUES (:what, :where, :table_name)",
-            what = what,
-            where = where,
-            table_name = table_name)
-
-    # Move jobs from memory to table
-    for job in tqdm(vacancies):
-        db.execute("INSERT INTO :table_name ("
-                    "title, company, salary_raw, salary_min, salary_max, salary_mid, location, latitude, longitude, url, description) "
-                "VALUES ("
-                    ":title, :company, :salary_raw, :salary_min, :salary_max, :salary_mid, :location, :latitude, :longitude, :link, :description);",
-                table_name = table_name,
-                title = job["title"],
-                company = job["company"],
-                salary_raw = job["salary_raw"],
-                salary_min = job["salary_min"],
-                salary_max = job["salary_max"],
-                salary_mid = job["salary_mid"],
-                location = job["location"],
-                latitude = job["latitude"],
-                longitude = job["longitude"],
-                link = job["link"],
-                description = job["description"])
-
-    print ("Table " + table_name + " created successfully.  Find your job vacancies there :-)")
-
-    return(table_name)
+            pass
 
 if __name__ == "__main__":
-    main()
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser(description = 'A web scraper for Indeed.com.')
+
+    # Required positional argument
+    parser.add_argument("what", help="String to search for.  E.g., 'Python developer'")
+    parser.add_argument("where", help="City or location to sarch in.  E.g. 'London'")
+
+    # Optional argument flag which defaults to False
+    parser.add_argument("-g", "--geocode", action="store_true", default=False, help="Attempt to find location of job vacancy by company name, location and city.  Requires environmental variable 'API_KEY' to be set to a Google Maps credential.")
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity (-v, -vv, etc)")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+
+    if args.geocode == True:
+        try:
+            API_KEY = os.environ['API_KEY']
+        except KeyError:
+            print("Environmental variable API_KEY must be set to a Google Maps credential for geocoding.")
+            sys.exit("Try running again with API_KEY set, or remove '-g' argument.")
+            
+    main(args)
